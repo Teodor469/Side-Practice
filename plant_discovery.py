945cb0de6190a954617fdb6a28def253fcbffd20
Gruka469 <lifetroll6@gmail.com>
2023-11-17 16:33:35 +0200

    solved

--- plant_discovery.py
+++ plant_discovery.py
@@ -1,45 +1,60 @@
-# Initialize a dictionary to store plant information
-plants = {}
+class PlantCollection:
+    def __init__(self) -> None:
+        self.plants = {}
 
-# Read the number of plants
+    def rate_plant(self, plant, rating):
+        if plant in self.plants:
+            self.plants[plant]['ratings'].append(int(rating))
+        else:
+            print("error")
+
+    def update_rarity(self, plant, new_rarity):
+        if plant in self.plants:
+            self.plants[plant]['rarity'] = int(new_rarity)
+        else:
+            print("error")
+
+    def reset(self, plant):
+        if plant in self.plants:
+            self.plants[plant]['ratings'] = []
+        else:
+            print("error")
+
+    def exhibition(self):
+        print("Plants for the exhibition:")
+        for plant, data in self.plants.items():
+            rarity = data['rarity']
+            ratings = data['ratings']
+            average_rating = sum(ratings) / len(ratings) if ratings else 0
+            print(f"- {plant}; Rarity: {rarity}; Rating: {average_rating:.2f}")
+
+
+plants = PlantCollection()
 n = int(input())
+commands = []
 
-# Input plant information and store it in the dictionary
 for _ in range(n):
     plant_info = input().split('<->')
     plant_name, rarity = plant_info[0], int(plant_info[1])
-    plants[plant_name] = {'rarity': rarity, 'ratings': []}
+    plants.plants[plant_name] = {'rarity': rarity, 'ratings': []}
 
-# Process commands
 while True:
-    command = input()
-    if command == "Exhibition":
-        break
-    tokens = command.split(' ')
-    main_command = tokens[0]
-    plant_name = tokens[1]
-    if main_command == "Rate:":
-        _, rating = tokens[2].split(" - ")
-        if plant_name in plants:
-            plants[plant_name]['ratings'].append(int(rating))
-        else:
-            print("error")
-    elif main_command == "Update:":
-        _, new_rarity = tokens[2].split(" - ")
-        if plant_name in plants:
-            plants[plant_name]['rarity'] = int(new_rarity)
-        else:
-            print("error")
-    elif main_command == "Reset:":
-        if plant_name in plants:
-            plants[plant_name]['ratings'] = []
-        else:
-            print("error")
+    command = input().split()
+    action = command[0]
 
-# Calculate average ratings and print the exhibition information
-print("Plants for the exhibition:")
-for plant_name, info in plants.items():
-    rarity = info['rarity']
-    ratings = info['ratings']
-    average_rating = 0 if not ratings else sum(ratings) / len(ratings)
-    print(f"- {plant_name}; Rarity: {rarity}; Rating: {average_rating:.2f}")
+    if action == 'Exhibition':
+        break
+    else:
+        commands.append(command)
+
+for command in commands:
+    action = command[0]
+
+    if action == 'Rate:':
+        plants.rate_plant(command[1], command[3])
+    elif action == 'Update:':
+        plants.update_rarity(command[1], command[3])
+    elif action == 'Reset:':
+        plants.reset(command[1])
+
+plants.exhibition()

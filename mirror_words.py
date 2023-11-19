fb2427c58e91b9299093fa376921c6f0bbdbb0cd
Gruka469 <lifetroll6@gmail.com>
2023-11-19 17:59:32 +0200

    Update mirror_words.py

--- mirror_words.py
+++ mirror_words.py
@@ -1,36 +1,21 @@
 import re
-
-def find_mirror_words(text):
-    pattern = re.compile(r'[@#]([A-Za-z]{3,})[^A-Za-z]+([A-Za-z]{3,})[@#]')
-    matches = pattern.findall(text)
-
-    valid_pairs = []
-    mirrored_pairs = []
-
-    for match in matches:
-        word_one, word_two = match
-        if word_one == word_two[::-1]:
-            mirrored_pairs.add(match)
-        elif word_one.lower() == word_two[::-1].lower():
-            valid_pairs.append(match)
-
-    return valid_pairs, mirrored_pairs
-
-def main():
-    text = input().strip()
-
-    valid_pairs, mirrored_pairs = find_mirror_words(text)
-
-    if not valid_pairs:
-        print("No word pairs found!")
-    else:
-        print(f"{len(valid_pairs)} word pairs found!")
-
-    if not mirrored_pairs:
-        print("No mirror words!")
-    else:
-        print("The mirror words are:")
-        print(' <=> '.join(' <=> '.join(pair) for pair in sorted(mirrored_pairs, key=lambda x: x[0])))
-
-if __name__ == "__main__":
-    main()
+command = input()
+list_matches = []
+new_list_matches = []
+pattern = r"([@#])([A-Za-z]{3,})(\1)(\1)([A-Za-z]{3,})(\1)"
+matches = re.findall(pattern,command)
+for match in matches:
+    list_matches.append(match[1])
+    list_matches.append(match[4])
+if len(list_matches) != 0:
+    print(f"{int((len(list_matches))/2)} word pairs found!")
+else:
+    print(f"No word pairs found!")
+for i in range (1,len(list_matches),2):
+    if (list_matches[i][::-1]) == (list_matches[i-1]) and (list_matches[i-1][::-1]) == (list_matches[i]):
+        new_list_matches.append(list_matches[i-1]+" <=> " + list_matches[i])
+if len(new_list_matches) > 0:
+    print("The mirror words are:")
+    print(', '.join(new_list_matches))
+else:
+    print("No mirror words!")

c06e1c578c6def03c0ee1d864437aa1ead738ebe
Gruka469 <lifetroll6@gmail.com>
2023-11-24 11:06:32 +0200

    Update soft_uni_exam11.py

--- soft_uni_exam11.py
+++ soft_uni_exam11.py
@@ -1,15 +1,17 @@
 n = int(input())
-word = input()
+magic_word = input()
 
-string_list = []
+strings = []
 
 for _ in range(n):
-    input_string = input()
-    string_list.append(input_string)
-print(string_list)
+    string = input()
+    strings.append(string)
+
+print(strings)
 
 filtered_strings = []
-for current_string in string_list:
-    if word in string_list:
+
+for current_string in strings:
+    if magic_word in current_string:
         filtered_strings.append(current_string)
-print(current_string)
+print(filtered_strings)

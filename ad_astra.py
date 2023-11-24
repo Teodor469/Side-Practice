c9535820ac7ba1f984c572c3df9e6a793d88f23d
Gruka469 <lifetroll6@gmail.com>
2023-11-24 20:43:16 +0200

    .

--- ad_astra.py
+++ ad_astra.py
@@ -1,9 +1,11 @@
 import re
+ 
 information = input()
-pattern = r'(?i)([#|])([a-z\s]+)(\1)(\d{2}\/\d{2}\/\d{2})(\1)(\d+)(\1)'
+pattern = r"(?i)([#|])([a-z\s]+)(\1)(\d{2}\/\d{2}\/\d{2})(\1)(\d+)(\1)"
+# index 1 - item name, index 3 - expiration date, index 5 - calories
 matches = re.findall(pattern, information)
 total_calories = sum([int(match[5]) for match in matches])
-days = total_calories //2000
+days = total_calories // 2000
 print(f"You have food to last you for: {days} days!")
 for element in matches:
     item_name = element[1]

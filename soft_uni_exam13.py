324082bb9bc432902f9dda7bb0643bfe82b9c0bf
Gruka469 <lifetroll6@gmail.com>
2023-11-24 11:57:49 +0200

    Update soft_uni_exam13.py

--- soft_uni_exam13.py
+++ soft_uni_exam13.py
@@ -3,6 +3,6 @@ sequence_of_numbers = input().split()
 abs_list = []
 
 for number in sequence_of_numbers:
-    abs_list.append(abs(float(sequence_of_numbers)))
+    abs_list.append(abs(float(number)))
 
 print(abs_list)

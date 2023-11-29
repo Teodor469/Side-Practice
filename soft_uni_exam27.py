db418ea4b7d8e027f3f5593d1d495eb42234ba00
Gruka469 <lifetroll6@gmail.com>
2023-11-29 13:16:19 +0200

    Update soft_uni_exam27.py

--- soft_uni_exam27.py
+++ soft_uni_exam27.py
@@ -1,11 +1,52 @@
-class MusicCollection:
+class Library:
     def __init__(self) -> None:
-        self.collection = {}
+        self.book_collection = {}
 
-    def add_piece(self, piece, composer, key, print_message = True):
-        if piece in self.collection:
-            print(f"{piece} is already in the collection!")
+    def add_book(self, title, author, genre):
+        if title in self.book_collection:
+            print(f"{title} is already in the library!")
         else:
-            self.collection[piece] = {"compser": composer, "key": key}
-            if print_message:
-                print(f"{piece} by {composer} in {key} added to the collection!")
+            self.book_collection[title] = {'author': author, 'genre': genre}
+            print(f"{title} by {author} in {genre} added to the library!")
+
+    def remove_book(self, title):
+        if title in self.book_collection:
+            del self.book_collection[title]
+            print(f"Successfully removed {title}!")
+        else:
+            print(f"Invalid operation! {title} does not exist in the library.")
+
+    def change_genre(self, title, new_genre):
+        if title in self.book_collection:
+            self.book_collection[title]['genre'] = new_genre
+            print(f"Changed the genre of {title} to {new_genre}!")
+        else:
+            print(f"Invalid operation! {title} does not exist in the library.")
+
+# Read the number of initial books
+number_of_initial_books = int(input())
+book_collection = Library()
+
+# Read and process each command until "Stop" is encountered
+for _ in range(number_of_initial_books):
+    book_info = input().split("|")
+    book_collection.add_book(book_info[0], book_info[1], book_info[2])
+
+while True:
+    command = input().split("|")
+
+    if command[0] == 'Stop':
+        break
+
+    action, title = command[0], command[1]
+
+    if action == 'Add':
+        book_collection.add_book(title, command[2], command[3])
+    elif action == 'Remove':
+        book_collection.remove_book(title)
+    elif action == "ChangeGenre":
+        book_collection.change_genre(title, command[2])
+
+# Print the final collection
+for title, info in book_collection.book_collection.items():
+    print(f"{title} -> Author: {info['author']}, Genre: {info['genre']}")
